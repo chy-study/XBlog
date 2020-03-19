44a0f318454b42bdf17bc1a0265a9ab079307191
CHY <1919216404@qq.com>
2020-03-19 22:25:15 +0800

    '新建springboot+vue项目'

---
+++ xblog-server/src/main/java/springboot/xblogserver/XblogServerApplication.java
@@ -0,0 +1,13 @@
+package springboot.xblogserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XblogServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XblogServerApplication.class, args);
+    }
+
+}
